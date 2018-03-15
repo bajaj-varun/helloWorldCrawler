c8fdf708e2e67e791935536aeef25fdd1abbe3ea
Varun Bajaj <bajaj.varun@hotmail.com>
2018-03-15 07:40:15 -0700

    Initial version

---
+++ Pharma/spiders/pharma.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+#from scrapy.selector import Selector
+
+class PharmaSpider(scrapy.Spider):
+    name = 'pharma'
+    #allowed_domains = ['pharma']
+    start_urls = [
+    	'https://www.pfizer.com/news',
+    ]
+
+    def parse(self, response):
+        print ("URL ", response.url) 
+        
+        for links in response.xpath('//a'):
+            link = links.xpath('@href').extract_first()
+            txt  = links.xpath('text()').extract_first()
+            
+            if txt is not None:
+                txt = txt.encode("utf-8").strip()
+                
+            print('Link', link.encode("utf-8"), 'Text', txt)
+            
