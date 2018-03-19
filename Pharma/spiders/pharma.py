c731a5514f74d8c41595f5f98af4601f611045fd
Varun Bajaj <bajaj.varun@hotmail.com>
2018-03-19 11:58:15 +0530

    regex updates

--- Pharma/spiders/pharma.py
+++ Pharma/spiders/pharma.py
@@ -1,25 +1,40 @@
 # -*- coding: utf-8 -*-
 import scrapy
-#from scrapy.selector import Selector
+import re
+
+# from scrapy.selector import Selector
 
 class PharmaSpider(scrapy.Spider):
     name = 'pharma'
-    #allowed_domains = ['pharma']
+    # allowed_domains = ['pharma']
     start_urls = [
-    	'https://www.pfizer.com/news',
+        'https://www.pfizer.com/news',
     ]
 
+    aList = []
+    mList = []
+    KW = r"news|media|person|people|invest|science|health|partner*|research"
+
     def parse(self, response):
-        print ("URL ", response.url) 
-        
+        print("URL %s" %response.url)
+
         for links in response.xpath('//a'):
             link = links.xpath('@href').extract_first()
-            txt  = links.xpath('text()').extract_first()
-            
+            txt = links.xpath('text()').extract_first()
+
             if txt is not None:
                 txt = txt.encode("utf-8").strip()
-                
-            print('Link', link.encode("utf-8"), 'Text', txt)
 
-            #links.xpath('//a[text()[re:test(.,"news|media|person|people|invest|science|health|partner*|research")]]').extract()
-            
+            lnk = link.encode("utf-8")
+            # Print all the links
+            print('Link', lnk, 'Text', txt)
+            self.aList.append(lnk)
+
+            if self.isKWExist(lnk):
+                self.mList.append(lnk)
+
+        print("Total number of records = %d" % self.aList.__len__())
+        print("Total matching records = %d" % self.mList.__len__())
+
+    def isKWExist(self, seed):
+        return re.search(self.KW, seed, flags=re.IGNORECASE)
